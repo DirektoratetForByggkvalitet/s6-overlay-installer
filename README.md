be5868e6f67a40f459a899ebf73e6b31663f3267
Eirik Wulff <ew@dibk.no>
2024-03-05 09:49:04 +0100

    Info about using the version variable added.

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ The script will determine the platform and install the correct binaries from the
 Use the following code in your Dockerfile in order to install S6-overlay:
 
 ```
+ENV S6_OVERLAY_VERSION="3.1.6.2" # Change when you need to update S6-overlay
 ADD https://bitbucket.org/dibk/s6-overlay-installer.git /s6-overlay-installer
 RUN /s6-overlay-installer/s6_install.sh && rm -rf /s6-overlay-installer
 ```
@@ -19,3 +20,5 @@ The script requires bash in order to run. Some images might require bash to be i
 ```
 RUN apk add --no-cache bash
 ```
+
+The environment variable S6_OVERLAY_VERSION will be needed in order to install versions of S6-overlay beyond the version set as default in the script (currently 3.1.6.2).
--- s6_install.sh
+++ s6_install.sh
@@ -3,7 +3,7 @@
 : "${S6_OVERLAY_VERSION:=3.1.6.2}"
 
 arch=$(uname -m)
-echo "Installerer S6-overlay, v${S6_OVERLAY_VERSION}, for ${arch}"
+echo "Installing S6-overlay, v${S6_OVERLAY_VERSION}, for ${arch}"
 
 # Endrer på filnavnet etter arkitektur.
 if [ "$arch" = "arm64" ]; then
@@ -17,9 +17,9 @@ fi
 declare -a files=("s6-overlay-${code}.tar.xz" "s6-overlay-noarch.tar.xz" "s6-overlay-symlinks-noarch.tar.xz" "s6-overlay-symlinks-arch.tar.xz")
 for file in "${files[@]}"
 do
-    echo "Behandler '${file}'"
+    echo "Processing '${file}'"
     curl -Lso /tmp/${file} https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/${file}
     tar -C / -Jxpf /tmp/${file}
 done
 
-echo "S6-overlay installert"
+echo "S6-overlay installed"
